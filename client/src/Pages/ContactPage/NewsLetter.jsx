d73c78310a4097a35259888bb2061385eee723a2
Idba1 <moniraislamself@gmail.com>
2025-02-07 17:23:17 +0600

    update newsletter

--- client/src/Pages/ContactPage/NewsLetter.jsx
+++ client/src/Pages/ContactPage/NewsLetter.jsx
@@ -1,7 +1,72 @@
+import { useState } from 'react';
+import Swal from 'sweetalert2';
+
 const Newsletter = () => {
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubscribe = async () => {
+        try {
+            const response = await fetch('https://demo.vercel.app/subscribe', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email }),
+            });
+            const data = await response.json();
+            if (response.ok) {
+                setMessage(data.message);
+            } else {
+
+                Swal.fire({
+                    title: "update!",
+                    text: data.error,
+                    imageUrl: "https://i.ibb.co/Fn1PqGq/download-5.png",
+                    imageWidth: 400,
+                    imageHeight: 200,
+                    imageAlt: "Custom image"
+                });
+            }
+        } catch (error) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'An error occurred. Please try again.',
+            });
+            console.log(error);
+        }
+    };
+
     return (
-        <div>
-            <h1>newsLetter</h1>
+        <div className="lg:my-20 my-10">
+            <section className="py-6 dark:bg-gray-100 dark:text-gray-900">
+                <div className="container mx-auto flex flex-col justify-center p-4 space-y-8 md:p-10 lg:space-y-0 lg:space-x-12 lg:justify-between lg:flex-row">
+                    <div className="flex flex-col space-y-4 text-center lg:text-left">
+                        <h1 className="lg:text-5xl text-3xl font-bold leading-none">Stay Informed with RiseWithYou Newsletter!</h1>
+                        <p className="lg:text-lg text-base">Subscribe to RiseWithYou newsletter to receive the latest updates on job listings, career advice, industry insights, and exclusive promotions directly to your inbox.</p>
+                    </div>
+                    <div className="flex flex-row items-center self-center justify-center flex-shrink-0 shadow-md lg:justify-end">
+                        <div className="flex flex-row">
+                            <input
+                                type="text"
+                                placeholder="example@email.com"
+                                className="w-3/5 p-3 rounded-l-lg sm:w-2/3"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <button
+                                type="button"
+                                className="self-center font-bold px-8 py-3 text-lg rounded focus:ring hover:ring focus:ring-opacity-75 hover:dark:border-sky-950 hover:dark:bg-sky-100 hover:dark:text-sky-950 dark:text-gray-50 focus:dark:ring-sky-200 dark:ring-sky-950 dark:bg-sky-800"
+                                onClick={handleSubscribe}
+                            >
+                                Subscribe
+                            </button>
+                        </div>
+                    </div>
+                    {message && <p>{message}</p>}
+                </div>
+            </section>
         </div>
     );
 };
