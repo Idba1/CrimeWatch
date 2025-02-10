703e9284cd8657634a909f465bd39df7365d71db
Idba1 <moniraislamself@gmail.com>
2025-02-10 23:38:02 +0600

    update

--- server/index.js
+++ server/index.js
@@ -3,15 +3,32 @@ const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 9000
 const app = express()
-const corsoptions = {
+const corsOptions = {
     origin: ['http://localhost:5173/', 'http://localhost:5174/'],
     Credential: true,
     optionSuccessStatus: 2000,
 }
-app.use(cors(corsoptions))
+app.use(cors(corsOptions))
 app.use(express.json())
+const uri = `mongodb+srv://solosphere:iWVwKAPVokeFjwvl@cluster0.kfk05.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    }
+}
+run().catch(console.dir);
 app.get('/', (req, res) => {
-    res.send("hello from CrimeWatch server..!!");
+    res.send("hello from solosphere server..!!");
 })
+
 app.listen(port, () =>
     console.log(`server running port on ${port}`))
