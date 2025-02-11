1bf0f26f4603b3fc279fb35e866276d4ddeb1f0a
Idba1 <moniraislamself@gmail.com>
2025-02-11 08:04:10 +0600

    register update for users collection

--- client/src/Pages/Authentication/Register.jsx
+++ client/src/Pages/Authentication/Register.jsx
@@ -1,12 +1,12 @@
 import { useContext, useEffect } from "react"
 import toast from "react-hot-toast"
 import { Link, useLocation, useNavigate } from "react-router-dom"
-import { AuthContext } from "../../Provider/AuthProvider"
 import axios from "axios"
+import { AuthContext } from "../../Provider/AuthProvider"
 
 const Registration = () => {
   const navigate = useNavigate()
-  const { signInWithGoogle, createUser, updateUserProfile, user, setUser, loading } = useContext(AuthContext)
+  const { createUser, updateUserProfile, user, setUser, loading } = useContext(AuthContext)
 
   const location = useLocation()
   const from = location.state || '/'
@@ -18,57 +18,37 @@ const Registration = () => {
   }, [navigate, user])
 
   const handleSignUp = async e => {
-    e.preventDefault()
-    const form = e.target
-    const email = form.email.value
-    const name = form.name.value
-    const photo = form.photo.value
-    const pass = form.password.value
-    console.log({ email, pass, name, photo })
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const name = form.name.value;
+    const photo = form.photo.value;
+    const pass = form.password.value;
+    console.log({ email, pass, name, photo });
+
     try {
+      // User Registration
+      const result = await createUser(email, pass);
+      await updateUserProfile(name, photo);
+      setUser({ ...result?.user, photoURL: photo, displayName: name });
 
-      //User Registration
-      const result = await createUser(email, pass)
-      // console.log(result)
-      await updateUserProfile(name, photo)
-      setUser({ ...result?.user, photoURL: photo, displayName: name })
-
-      // console.log(result.user);
-      const { data } = await axios.post(
-        `${import.meta.env.VITE_API_URL}/jwt`,
-        { email: result?.user?.email },
+      const response = await axios.post(
+        `http://localhost:9000/users`,
+        { email, pass, name, photo },
         { withCredentials: true }
       );
 
-      console.log(data);
-      navigate(from, { replace: true })
-      toast.success('Signup Successful')
-    } catch (err) {
-      console.log(err)
-      toast.error(err?.message)
-    }
-  }
+      console.log(response.data);
 
-  // Google 
-  const handleGoogleSignIn = async () => {
-    try {
-      const result = await signInWithGoogle()
-      console.log(result.user);
-      const { data } = await axios.post(
-        `${import.meta.env.VITE_API_URL}/jwt`,
-        { email: result?.user?.email },
-        { withCredentials: true }
-      );
-
-      console.log(data);
-      toast.success('Signin Successful')
-      navigate(from, { replace: true })
+      navigate(from, { replace: true });
+      toast.success('Signup Successful');
     } catch (err) {
-      console.log(err)
-      toast.error(err?.message)
+      console.log(err);
+      toast.error(err?.message);
     }
-  }
-  if (user || loading) return
+  };
+
+  if (user || loading) return null;
 
   return (
     <div className='flex justify-center items-center my-12 min-h-[calc(100vh-306px)]'>
@@ -85,8 +65,8 @@ const Registration = () => {
           <p className='mt-3 text-xl text-center text-gray-600 '>
             Get Your Free Account Now.
           </p>
-
-          <div onClick={handleGoogleSignIn} className='flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg   hover:bg-gray-50 '>
+          {/* 
+          <div onClick={handleGoogleSignIn} className='flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg hover:bg-gray-50'>
             <div className='px-4 py-2'>
               <svg className='w-6 h-6' viewBox='0 0 40 40'>
                 <path
@@ -111,12 +91,12 @@ const Registration = () => {
             <span className='w-5/6 px-4 py-3 font-bold text-center'>
               Sign in with Google
             </span>
-          </div>
+          </div> */}
 
           <div className='flex items-center justify-between mt-4'>
-            <span className='w-1/5 border-b  lg:w-1/4'></span>
+            <span className='w-1/5 border-b lg:w-1/4'></span>
 
-            <div className='text-xs text-center text-gray-500 uppercase  hover:underline'>
+            <div className='text-xs text-center text-gray-500 uppercase hover:underline'>
               or Registration with email
             </div>
 
@@ -125,7 +105,7 @@ const Registration = () => {
           <form onSubmit={handleSignUp}>
             <div className='mt-4'>
               <label
-                className='block mb-2 text-sm font-medium text-gray-600 '
+                className='block mb-2 text-sm font-medium text-gray-600'
                 htmlFor='name'
               >
                 Username
@@ -134,13 +114,13 @@ const Registration = () => {
                 id='name'
                 autoComplete='name'
                 name='name'
-                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
+                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300'
                 type='text'
               />
             </div>
             <div className='mt-4'>
               <label
-                className='block mb-2 text-sm font-medium text-gray-600 '
+                className='block mb-2 text-sm font-medium text-gray-600'
                 htmlFor='photo'
               >
                 Photo URL
@@ -149,13 +129,13 @@ const Registration = () => {
                 id='photo'
                 autoComplete='photo'
                 name='photo'
-                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
+                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300'
                 type='text'
               />
             </div>
             <div className='mt-4'>
               <label
-                className='block mb-2 text-sm font-medium text-gray-600 '
+                className='block mb-2 text-sm font-medium text-gray-600'
                 htmlFor='LoggingEmailAddress'
               >
                 Email Address
@@ -164,7 +144,7 @@ const Registration = () => {
                 id='LoggingEmailAddress'
                 autoComplete='email'
                 name='email'
-                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
+                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300'
                 type='email'
               />
             </div>
@@ -172,7 +152,7 @@ const Registration = () => {
             <div className='mt-4'>
               <div className='flex justify-between'>
                 <label
-                  className='block mb-2 text-sm font-medium text-gray-600 '
+                  className='block mb-2 text-sm font-medium text-gray-600'
                   htmlFor='loggingPassword'
                 >
                   Password
@@ -183,7 +163,7 @@ const Registration = () => {
                 id='loggingPassword'
                 autoComplete='current-password'
                 name='password'
-                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
+                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300'
                 type='password'
               />
             </div>
